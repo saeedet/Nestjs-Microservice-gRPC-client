b83bff778160e332472a5189dd8293418af7c2ae
Saeed <saeed_et90@yahoo.com>
2022-01-05 14:14:16 +1100

    grpc setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HeroModule } from './hero/hero.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    HeroModule,
   ],
   controllers: [AppController],
   providers: [AppService],
